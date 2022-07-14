4e85c3add0bba3d5df96cd4c2d7b41d7fa3c3046
htm <destokp@gmail.com>
2022-07-14 00:53:49 -0400

    make it betterssts

--- index.js
+++ index.js
@@ -15,22 +15,6 @@
   console.log("Running at Port"+PORT);
 
 
-  const accountSid = process.env.TWILIO_ACCOUNT_SID;
-  const authToken = process.env.TWILIO_AUTH_TOKEN;
-  const client = require('twilio')(accountSid, authToken);
-  let img_sen =function(req,res){
-    client.messages
-  .create({
-     mediaUrl: ['https://images.unsplash.com/photo-1545093149-618ce3bcf49d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80'],
-     from: 'whatsapp:+14155238886',
-     to: 'whatsapp:+51952815183'
-   })
-  .then(message => console.log(message.sid));
-    res.json({ mensaje: '¡sent!' })  
-  }
-
-
-
   let alumst =function(req,res){
   console.log('hol')
   res.json({ mensaje: '¡sent!' })  
@@ -48,5 +32,5 @@
   */
   }
   
-  app.get('/img',img_sen);
+
   app.get('/alums',alumst);
